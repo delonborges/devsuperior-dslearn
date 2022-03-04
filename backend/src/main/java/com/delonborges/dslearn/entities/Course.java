2cf878077b1a9c7295b943c51d9c58fb22b27aa4
Delon Borges <delon.borges@icloud.com>
2022-03-03 22:47:50 -0300

    Add Course entity and courses inserts

---
+++ backend/src/main/java/com/delonborges/dslearn/entities/Course.java
@@ -0,0 +1,25 @@
+package com.delonborges.dslearn.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_course")
+public class Course implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String imgUri;
+    private String imgGrayUri;
+}
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp Spring Boot', 'https://imgur.com/bdqES46', 'https://imgur.com/rDH6HWv');
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp React', 'https://imgur.com/Oc2Qbik', 'https://imgur.com/1qdor18');
